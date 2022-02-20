39881cc813158e7302716213d8d9f126dafc8929
Jonas B <emoji@mail.de>
2022-02-20 20:16:36 +0100

    Add ManageFavorites

---
+++ Better Books/Views/ManageFavorites.swift
@@ -0,0 +1,20 @@
+//
+//  ManageFavorites.swift
+//  Better Books
+//
+//  Created by Jonas on 20.02.22.
+//
+
+import SwiftUI
+
+struct ManageFavorites: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ManageFavorites_Previews: PreviewProvider {
+    static var previews: some View {
+        ManageFavorites()
+    }
+}
