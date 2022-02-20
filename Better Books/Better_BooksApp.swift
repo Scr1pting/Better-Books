3c8ac2bb3bf9912fb9c7edbcfb21052b4fe958a2
Jonas B <emoji@mail.de>
2022-02-20 15:08:32 +0100

    Initial Commit

---
+++ Better Books/Better_BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_BooksApp.swift
+//  Better Books
+//
+//  Created by Jonas on 20.02.22.
+//
+
+import SwiftUI
+
+@main
+struct Better_BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
