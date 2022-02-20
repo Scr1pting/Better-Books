58852bd4d5232aed8a749e8b4b3f4c786f16b727
Jonas B <emoji@mail.de>
2022-02-20 16:25:00 +0100

    Add special font headline

--- Better Books/Better_BooksApp.swift
+++ Better Books/Better_BooksApp.swift
@@ -9,6 +9,14 @@ import SwiftUI
 
 @main
 struct Better_BooksApp: App {
+    
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.font : UIFont(name: "Georgia-Bold", size: 32)!]
+        
+        UINavigationBar.appearance().titleTextAttributes = [.font : UIFont(name: "Georgia-Bold", size: 19)!]
+
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
